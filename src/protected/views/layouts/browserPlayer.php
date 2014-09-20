06ccd753d4ca568b23bdf22bf29830df65cdaa40
Sam Stenvall <neggelandia@gmail.com>
2014-09-20 14:26:43 +0300

    add a simple mechanism for playing items directly in the browser (fixes #208)

---
+++ src/protected/views/layouts/browserPlayer.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<title><?php echo strip_tags($this->pageTitle); ?></title>
+		<meta charset="utf-8" />
+		<meta name="viewport" content="width=device-width, initial-scale=0.65" />
+		<style type="text/css">
+			body {
+				margin: 0;
+				padding: 0;
+				background: #000;
+			}
+			
+			video {
+				width: 100%;
+				height: 100%;
+			}
+		</style>
+	</head>
+	
+	<body>
+		<?php echo $content; ?>
+	</body>
+	
+</html>
